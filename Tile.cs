11c6a851451ec2b434110f86ca98c2c331e3508c
HAndy2013 <wybpp2013@hotmail.com>
2017-12-20 14:52:10 +0800

    删除多余引用

--- Tile.cs
+++ Tile.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Game2048
+﻿namespace Game2048
 {
     public class Tile : BaseObj
     {
